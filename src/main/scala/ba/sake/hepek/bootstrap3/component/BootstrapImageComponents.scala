6084bf7065faa87b76ed4a43e6f7d1c7b53d7c2d
Sakib Hadžiavdić <sake.etf@gmail.com>
2018-09-11 19:19:02 +0200

    No thumbnail class when no image caption

--- src/main/scala/ba/sake/hepek/bootstrap3/component/BootstrapImageComponents.scala
+++ src/main/scala/ba/sake/hepek/bootstrap3/component/BootstrapImageComponents.scala
@@ -8,21 +8,27 @@ trait BootstrapImageComponents {
 
   /** Renders a pretty image with optional caption text */
   def image(source: String, caption: String = "") =
-    div(cls := "thumbnail")(
-      img(src := source, cls := "img-responsive", alt := caption), {
-        if (caption.trim.isEmpty) frag()
-        else div(cls := "caption text-center")(p(caption))
-      }
-    )
+    if (caption.trim.isEmpty) {
+      img(src := source, cls := "img-responsive", alt := caption)
+    } else {
+      div(cls := "thumbnail")(
+        img(src := source, cls := "img-responsive", alt := caption),
+        div(cls := "caption text-center")(p(caption))
+      )
+    }
 
   /** Renders a pretty bootstrapy SVG image with optional caption text */
-  def svg(source: String, captionn: String = "") =
-    div(cls := "thumbnail")(
+  def svg(source: String, caption: String = "") =
+    if (caption.trim.isEmpty) {
       tag("object")(tpe := "image/svg+xml", cls := "embed-responsive-item", data := source)(
         "Problem with rendering SVG..."
-      ), {
-        if (captionn.trim.isEmpty) frag()
-        else div(cls := "caption text-center")(p(captionn))
-      }
-    )
+      )
+    } else {
+      div(cls := "thumbnail")(
+        tag("object")(tpe := "image/svg+xml", cls := "embed-responsive-item", data := source)(
+          "Problem with rendering SVG..."
+        ),
+        div(cls := "caption text-center")(p(caption))
+      )
+    }
 }
