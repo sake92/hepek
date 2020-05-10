e4e2a2af2091c01efe35e91cfd2486f8627027c4
Sakib Hadžiavdić <sake.etf@gmail.com>
2020-05-10 04:33:46 +0200

    cleanup grid

--- hepek-components/src/main/scala/ba/sake/hepek/html/component/GridComponents.scala
+++ hepek-components/src/main/scala/ba/sake/hepek/html/component/GridComponents.scala
@@ -1,40 +1,41 @@
 package ba.sake.hepek.html.component
 
 import scalatags.Text.all._
+import ba.sake.stone.Wither
 
 trait GridComponents {
   import GridComponents._
 
-  val screenRatios: ScreenRatios
+  val Companion = GridComponents
 
-  // utility for making a row with a single column
-  protected def mkRowSingleCol(content: Seq[Frag]): Frag
+  def screenRatios: ScreenRatios
 
-  protected def mkRow(content: Frag*): Frag
-
-  protected def mkCol2(index: Int, content: Col2): Frag
-  protected def mkCol3(index: Int, content: Col3): Frag
-
-  // API
   def row(content: Frag*): Frag =
     mkRowSingleCol(content)
 
-  def row(c1: Col2, c2: Col2): Frag =
+  def row(col1: Col2, col2: Col2): Frag =
     mkRow(
-      mkCol2(0, c1),
-      mkCol2(1, c2)
+      mkCol2(0, col1),
+      mkCol2(1, col2)
     )
 
-  def row(c1: Col3, c2: Col3, c3: Col3): Frag =
+  def row(col1: Col3, col2: Col3, col3: Col3): Frag =
     mkRow(
-      mkCol3(0, c1),
-      mkCol3(1, c2),
-      mkCol3(2, c3)
+      mkCol3(0, col1),
+      mkCol3(1, col2),
+      mkCol3(2, col3)
     )
 
   def half(content: Frag*): Col2 = Col2(content.toList)
 
   def third(content: Frag*): Col3 = Col3(content.toList)
+
+  /* constructors */
+  protected def mkRow(content: Frag*): Frag
+
+  protected def mkRowSingleCol(content: Seq[Frag]): Frag
+  protected def mkCol2(index: Int, content: Col2): Frag
+  protected def mkCol3(index: Int, content: Col3): Frag
 }
 
 object GridComponents {
@@ -47,8 +48,9 @@ object GridComponents {
     Option(Ratios.Default),
     Option(Ratios.Default)
   )
-  final case class Col2(content: List[Frag]) // 2 parts
-  final case class Col3(content: List[Frag]) // 3 parts
+
+  final case class Col2(content: List[Frag])
+  final case class Col3(content: List[Frag])
 
   final case class Ratio(values: List[Int])
 
@@ -56,6 +58,7 @@ object GridComponents {
     def apply(values: Int*): Ratio = new Ratio(values.toList)
   }
 
+  @Wither
   final case class Ratios(
       single: Ratio = Ratios.DefaultSingle,
       half: Ratio = Ratios.DefaultHalf,
@@ -84,24 +87,18 @@ object GridComponents {
     def withThird(r1: Int, r2: Int, r3: Int): Ratios = withThird(Ratio(r1, r2, r3))
   }
 
+  @Wither
   final case class ScreenRatios(
       lg: Ratios,
       md: Option[Ratios] = None,
       sm: Option[Ratios] = None,
       xs: Option[Ratios] = None
   ) {
-    def withLg(r: Ratios)         = copy(lg = r)
-    def withMd(r: Ratios)         = copy(md = Some(r))
-    def withMd(r: Option[Ratios]) = copy(md = r)
-    def withSm(r: Ratios)         = copy(sm = Some(r))
-    def withSm(r: Option[Ratios]) = copy(sm = r)
-    def withXs(r: Ratios)         = copy(xs = Some(r))
-    def withXs(r: Option[Ratios]) = copy(xs = r)
-    def withAll(r: Ratios)        = withLg(r).withMd(r).withSm(r).withXs(r)
+    def withAll(r: Ratios) = withLg(r).withMd(r).withSm(r).withXs(r)
   }
 
   object Ratios {
-    val DefaultSingle = Ratio(0, 1, 0) // 0:1:0, no padding
+    val DefaultSingle = Ratio(0, 1, 0) // no padding
     val DefaultHalf   = Ratio(1, 1)
     val DefaultThird  = Ratio(1, 1, 1)
     val Default       = Ratios(DefaultSingle, DefaultHalf, DefaultThird)
