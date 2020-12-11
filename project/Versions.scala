2f859fdd683b44b2c66560e6e0fa63a0ca4cbf17
Scala Steward <me@scala-steward.org>
2020-12-11 04:46:34 +0100

    Update commonmark to 0.16.1

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,7 +4,7 @@ object V {
   val upickle   = "1.2.1"
 
   val hepekCore     = "0.2.0"
-  val commonMark    = "0.15.2"
+  val commonMark    = "0.16.1"
   val plantUml      = "1.2020.18"
   val jsoup         = "1.12.2"
   val openHtmlToPdf = "1.0.2"
