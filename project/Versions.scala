1a63985d14d9240233ee556842f5e48d5f6c29cc
Scala Steward <scala_steward@virtuslab.com>
2023-03-10 19:10:07 +0000

    Update scalatags to 0.12.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,9 +1,9 @@
 object V {
-  val scalaTags = "0.11.1"
+  val scalaTags = "0.12.0"
   val tupson    = "0.5.1"
 
   val hepekCore     = "0.2.0"
-  val flexmark    = "0.62.2"
+  val flexmark      = "0.62.2"
   val plantUml      = "1.2023.2"
   val jsoup         = "1.15.4"
   val openHtmlToPdf = "1.0.2"
