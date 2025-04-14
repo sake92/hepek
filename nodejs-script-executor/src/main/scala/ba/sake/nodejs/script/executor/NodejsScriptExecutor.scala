0f526dd1fa881ecd5eab5051760f0a6c20529df6
Sakib Hadziavdic <sakib@sake.ba>
2025-04-14 14:24:58 +0200

    Cache nodejs script exec result on disk

--- nodejs-script-executor/src/main/scala/ba/sake/nodejs/script/executor/NodejsScriptExecutor.scala
+++ nodejs-script-executor/src/main/scala/ba/sake/nodejs/script/executor/NodejsScriptExecutor.scala
@@ -1,7 +1,10 @@
 package ba.sake.nodejs.script.executor
 
+import java.security.MessageDigest
 import scala.util.Properties
 import NodejsScriptExecutor.*
+
+import java.util.Base64
 
 /** Executes a Node.js script in a given folder (sandbox) with optional npm dependencies.
   * @param baseFolder
@@ -14,6 +17,14 @@ class NodejsScriptExecutor(
     dependencies: Seq[NpmDependency] = Seq.empty
 ) {
   initialize()
+
+  private def getMd5B64(str: String): String = {
+    val bytesOfMessage = str.getBytes("UTF-8")
+    val md             = MessageDigest.getInstance("MD5")
+    val theMD5digest   = md.digest(bytesOfMessage)
+    val b64 = Base64.getEncoder.encode(theMD5digest)
+    new String(b64, "UTF-8").replace('/', '-').replace('=','_').replace('+','$')
+  }
 
   private def initialize(): Unit = {
     val envKey = baseFolder.toNIO.toAbsolutePath.toString
@@ -28,8 +39,6 @@ class NodejsScriptExecutor(
     }
   }
 
-  // TODO cache overall results on disk!!! ???
-
   /** Executes a Node.js script in the given folder.
     * @param jsScript
     *   JavaScript code to be executed.
@@ -39,11 +48,21 @@ class NodejsScriptExecutor(
     *   stdout of the script
     */
   def executeScript(jsScript: String, scriptName: String = "script.js"): String = {
-    val scriptFile = baseFolder / scriptName
-    os.write.over(scriptFile, jsScript)
-    val res = os.call((nodeExe, scriptFile), cwd = baseFolder)
-    // remove ANSI codes from output https://stackoverflow.com/a/14652763/4496364
-    res.out.text().replaceAll("\u001B\\[[;\\d]*m", "")
+    val scriptMd5            = getMd5B64(jsScript)
+    val cachedResultFileName = baseFolder / "cached-results" / s"$scriptMd5.txt"
+    println(s"cachedResultFileName = $cachedResultFileName")
+    if os.exists(cachedResultFileName) then {
+      os.read(cachedResultFileName)
+    } else {
+      val scriptFile = baseFolder / scriptName
+      os.write.over(scriptFile, jsScript)
+      val res = os.call((nodeExe, scriptFile), cwd = baseFolder)
+      // remove ANSI codes from output https://stackoverflow.com/a/14652763/4496364
+      val finalResult = res.out.text().replaceAll("\u001B\\[[;\\d]*m", "")
+      // cache the result
+      os.write.over(cachedResultFileName, finalResult, createFolders = true)
+      finalResult
+    }
   }
 }
 
@@ -51,7 +70,7 @@ object NodejsScriptExecutor {
   private val npmExe  = if Properties.isWin then "npm.cmd" else "npm"
   private val nodeExe = if Properties.isWin then "node.exe" else "node"
 
-  // environment -> installed dependencies
+  // baseFolder -> installed dependencies
   private var installedDepsCache = Map[String, Set[NpmDependency]]()
 
   case class NpmDependency(name: String, version: Option[String] = None) {
