24678bc3ac1174b10a31d270c0d9e97691b2fa52
Sakib Hadziavdic <sakib@sake.ba>
2025-04-15 17:17:32 +0200

    Fix test

--- nodejs-script-executor/src/test/scala/ba/sake/nodejs/script/executor/NodejsScriptExecutorSpec.scala
+++ nodejs-script-executor/src/test/scala/ba/sake/nodejs/script/executor/NodejsScriptExecutorSpec.scala
@@ -1,31 +1,33 @@
 package ba.sake.nodejs.script.executor
 
-import ba.sake.nodejs.script.executor.NodejsScriptExecutor.*
+import ba.sake.nodejs.script.executor.NodejsScriptExecutor
+import ba.sake.nodejs.script.executor.NodejsScriptExecutor.NpmDependency
 
 class NodejsScriptExecutorSpec extends munit.FunSuite {
+  
   test("Hello NodeJS") {
-    val env = Environment(os.pwd / "tmp/hepek/tests/nodejs-hello")
-    val res = execute(
-      env,
+    val executor = NodejsScriptExecutor(os.pwd / "tmp/hepek/tests/nodejs-hello")
+    val res = executor.executeScript(
       """
         |console.log("Hello NodeJS!")
         |""".stripMargin
     )
     assertEquals(res.trim, "Hello NodeJS!")
   }
-//
+
   test("CommonJS module with deps (katex math highlighter)") {
-    val env = Environment(os.pwd / "tmp/hepek/tests/nodejs-katex")
-    val res = execute(
-      env,
+    val executor = NodejsScriptExecutor(
+      os.pwd / "tmp/hepek/tests/nodejs-katex",
+      Seq(NpmDependency("katex", Some("0.16.21")))
+    )
+    val res = executor.executeScript(
       """
          const katex = require('katex');
          
          const html = katex.renderToString('\Eta\Epsilon\pi\Epsilon\Kappa');
          
          console.log(html);
-       """,
-      Set(NpmDependency("katex", Some("0.16.21")))
+       """
     )
     val expected =
       """<span class="katex"><span class="katex-mathml"><math xmlns="http://www.w3.org/1998/Math/MathML"><semantics><mrow><mi>E</mi><mi>t</mi><mi>a</mi><mi>E</mi><mi>p</mi><mi>s</mi><mi>i</mi><mi>l</mi><mi>o</mi><mi>n</mi><mi>p</mi><mi>i</mi><mi>E</mi><mi>p</mi><mi>s</mi><mi>i</mi><mi>l</mi><mi>o</mi><mi>n</mi><mi>K</mi><mi>a</mi><mi>p</mi><mi>p</mi><mi>a</mi></mrow><annotation encoding="application/x-tex">EtaEpsilonpiEpsilonKappa</annotation></semantics></math></span><span class="katex-html" aria-hidden="true"><span class="base"><span class="strut" style="height:0.8889em;vertical-align:-0.1944em;"></span><span class="mord mathnormal">Et</span><span class="mord mathnormal">a</span><span class="mord mathnormal">Ep</span><span class="mord mathnormal">s</span><span class="mord mathnormal">i</span><span class="mord mathnormal" style="margin-right:0.01968em;">l</span><span class="mord mathnormal">o</span><span class="mord mathnormal">n</span><span class="mord mathnormal">p</span><span class="mord mathnormal">i</span><span class="mord mathnormal">Ep</span><span class="mord mathnormal">s</span><span class="mord mathnormal">i</span><span class="mord mathnormal" style="margin-right:0.01968em;">l</span><span class="mord mathnormal">o</span><span class="mord mathnormal">n</span><span class="mord mathnormal" style="margin-right:0.07153em;">K</span><span class="mord mathnormal">a</span><span class="mord mathnormal">pp</span><span class="mord mathnormal">a</span></span></span></span>"""
@@ -33,9 +35,11 @@ class NodejsScriptExecutorSpec extends munit.FunSuite {
   }
 
   test("ES6 module with deps (shiki highlighter)") {
-    val env = Environment(os.pwd / "tmp/hepek/tests/nodejs-shiki")
-    val res = execute(
-      env,
+    val executor = NodejsScriptExecutor(
+      os.pwd / "tmp/hepek/tests/nodejs-shiki",
+      Seq(NpmDependency("shiki", Some("3.2.1")))
+    )
+    val res = executor.executeScript(
       """
         import { codeToHtml } from 'shiki';
     
@@ -47,7 +51,6 @@ class NodejsScriptExecutorSpec extends munit.FunSuite {
         
         console.log(html);
       """,
-      Set(NpmDependency("shiki", Some("3.2.1"))),
       scriptName = "script.mjs" // must explicitly use .mjs for ES6 modules
     )
     val expected =
